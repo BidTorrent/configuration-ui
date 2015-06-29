877aa8eff3ea8dadcc22790dfb7caf60011f8739
Benoit Jehanno <b.jehanno@criteo.com>
2015-06-29 19:49:14 +0200

    Adding simple website with 2 views

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+ var bidTorrentApp = angular.module('bidTorrentApp', ['ui.router'])
+ .config(function($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/publisher");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('publisher', {
+            url: "/publisher",
+            templateUrl: "partials/publisher.html"
+        })
+        .state('bidder', {
+            url: "/bidder",
+            templateUrl: "partials/bidder.html"
+        })
+});
