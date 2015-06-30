8555067193aac80bb7deb0b1753b189c617d25bd
Benoit Jehanno <b.jehanno@criteo.com>
2015-06-30 11:58:43 +0200

    Add a home page
    * clean index.html from non-existing js dependency
    * remove .css file

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,14 @@ var bidTorrentApp = angular.module('bidTorrentApp', ['ui.router'])
 .config(function($stateProvider, $urlRouterProvider) {
 
     // For any unmatched url, redirect to /state1
-    $urlRouterProvider.otherwise("/publisher");
+    $urlRouterProvider.otherwise("/");
 
     // Now set up the states
     $stateProvider
+        .state('home', {
+            url: "/",
+            templateUrl: "partials/home.html"
+        })
         .state('publisher', {
             url: "/publisher",
             templateUrl: "partials/publisher.html"
