4046210d9b5781c5e8759d7ef36160f327369772
Benoit Jehanno <b.jehanno@criteo.com>
2015-06-30 18:42:42 +0200

    fix gruntfile to include the api in bin/ (for the moment use easyphp to serve)

--- README.md
+++ README.md
@@ -11,9 +11,10 @@ UI for the configuration website of bidTorrent: http://www.bidtorrent.io
 
 ## Run the website locally for dev
 
-* run `grunt serverwatch` and connect to `localhost:3000` for dev
+* -run `grunt serverwatch` and connect to `localhost:3000` for dev- (broken ATM use [easyphp](http://www.easyphp.org/))
+* or run [easyphp](http://www.easyphp.org/) on the src/ folder
 
 ## Package the app for release
 
 * run `grunt release` to package the app in the bin/ folder.
-* run `grunt connect` to test it locally
+* you can use [easyphp](http://www.easyphp.org/) on the bin/ folder to 
--- gruntfile.js
+++ gruntfile.js
@@ -1,31 +1,21 @@
 module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-browser-sync');
-    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Project configuration.
     grunt.initConfig({
-        // Host release version
-        connect: {
-            server: {
-                options: {
-                    port: 3000,
-                    base: 'bin',
-                    keepalive: true
-                }
-            }
-        },
         // Copy the needed file for release
         copy: {
             release: {
                 files: [
                     {
+                        dot: true, //copy hidden files
                         expand: true,
                         cwd: 'src/',
-                        src: ['index.html', 'partials/**/*.html', 'js/**/*.js', 'assets/css/main.css', 'vendors/**'],
+                        src: ['index.html', 'partials/**/*.html', 'js/**/*.js', 'assets/css/main.css', 'vendors/**', 'api/**'],
                         dest: 'bin'
                     }
                 ]
