10e9860e3061203c53428f5855741b4db05d9e6c
Benoit Jehanno <b.jehanno@criteo.com>
2015-06-30 17:50:48 +0200

    add 'grunt release' to package the app in the bin/ folder and 'grunt connect' to host it locally for test

--- gruntfile.js
+++ gruntfile.js
@@ -1,17 +1,49 @@
 module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Project configuration.
     grunt.initConfig({
+        // Host release version
+        connect: {
+            server: {
+                options: {
+                    port: 3000,
+                    base: 'bin',
+                    keepalive: true
+                }
+            }
+        },
+        // Copy the needed file for release
+        copy: {
+            release: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src/',
+                        src: ['index.html', 'partials/**/*.html', 'js/**/*.js', 'assets/css/main.css', 'vendors/**'],
+                        dest: 'bin'
+                    }
+                ]
+            }
+        },
         // Compile less files
         less: {
+            debug: {
+                options: {
+                },
+                files: {
+                    "src/assets/css/main.css": "src/assets/css/main.less"
+                }
+            },
             release: {
                 options: {
-/*                    cleancss: true,
-                    compress: true*/
+                    cleancss: true,
+                    compress: true
                 },
                 files: {
                     "src/assets/css/main.css": "src/assets/css/main.less"
@@ -41,7 +73,8 @@ module.exports = function(grunt) {
         }
     });
 
-    //task
+    // tasks
     grunt.registerTask('serverwatch', ['browserSync', 'watch']);
+    grunt.registerTask('release', ['less:release', 'copy:release']);
 
 };
