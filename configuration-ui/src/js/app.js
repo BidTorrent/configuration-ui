170e4ed8b62b163d73f252babf166f57238a490a
Benoit Jehanno <b.jehanno@criteo.com>
2015-07-01 15:36:54 +0200

    Added ng-oauth (not working atm we need https)

--- configuration-ui/src/js/app.js
+++ configuration-ui/src/js/app.js
@@ -2,10 +2,13 @@
 
 var btApp = angular.module('btApp', [
     'ui.router',
+    'oauth',
     'btApp.bidder',
     'btApp.publisher'
 ]).
-config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+    //Activate the HTML5 mode to catch the access token.
+    $locationProvider.html5Mode(true).hashPrefix('!')
 
     // For any unmatched url, redirect to /state1
     $urlRouterProvider.otherwise('/');
