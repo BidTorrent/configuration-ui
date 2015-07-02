dc450324b99dca420a8476f883a6ffbf32559107
Camille Coueslant <c.coueslant@criteo.com>
2015-07-02 14:26:23 +0200

    Fix publisher config ids

--- configuration-ui/src/js/controllers/publisher.js
+++ configuration-ui/src/js/controllers/publisher.js
@@ -97,8 +97,8 @@ angular.module('btApp.publisher', ['ui.router', 'ngResource'])
     var hashCode = function(str){
         var hash = 0;
         if (!str || str.length == 0) return hash;
-        for (i = 0; i < str.length; i++) {
-            char = str.charCodeAt(i);
+        for (var i = 0; i < str.length; i++) {
+            var char = str.charCodeAt(i);
             hash = ((hash<<5)-hash)+char;
             hash = hash & hash; // Convert to 32bit integer
         }
