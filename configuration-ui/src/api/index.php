9e2958ce2094db10ebdcea05103bfd23c5e0821d
Camille Coueslant <c.coueslant@criteo.com>
2015-07-03 15:42:27 +0200

    Revert "[ConfigUI] Update URLs used by the UI"
    This reverts commit c1177cf69eca0bf889a89882006d5b7e3d6cf9c9.

--- configuration-ui/src/api/index.php
+++ configuration-ui/src/api/index.php
@@ -8,18 +8,18 @@ $app = new \Slim\Slim();
 $app->config('debug', false);
 
 $bidders = new Bidders();
-$app->get('/bidders/?format=ui', function () use ($app, $bidders) { display_result($app, $bidders->getAll()); });
-$app->get('/bidders/:id?format=ui', function ($id) use ($app, $bidders) { display_result($app, $bidders->get($app, $id)); });
-$app->delete('/bidders/:id?format=ui', function ($id) use ($app, $bidders) { $bidders->delete($app, $id); });
-$app->put('/bidders/:id?format=ui', function ($id) use ($app, $bidders) { $bidders->put($app, $id); });
-$app->post('/bidders/?format=ui', function () use ($app, $bidders) { $bidders->post($app); });
+$app->get('/bidders/', function () use ($app, $bidders) { display_result($app, $bidders->getAll()); });
+$app->get('/bidders/:id', function ($id) use ($app, $bidders) { display_result($app, $bidders->get($app, $id)); });
+$app->delete('/bidders/:id', function ($id) use ($app, $bidders) { $bidders->delete($app, $id); });
+$app->put('/bidders/:id', function ($id) use ($app, $bidders) { $bidders->put($app, $id); });
+$app->post('/bidders/', function () use ($app, $bidders) { $bidders->post($app); });
 
 $publishers = new Publishers();
-$app->get('/publishers/?format=ui', function () use ($app, $publishers) { display_result($app, $publishers->getAll()); });
-$app->get('/publishers/:id?format=ui', function ($id) use ($app, $publishers) { display_result($app, $publishers->get($app, $id)); });
-$app->delete('/publishers/:id?format=ui', function ($id) use ($app, $publishers) { $publishers->delete($app, $id); });
-$app->put('/publishers/:id?format=ui', function ($id) use ($app, $publishers) { $publishers->put($app, $id); });
-$app->post('/publishers/?format=ui', function () use ($app, $publishers) { $publishers->post($app); });
+$app->get('/publishers/', function () use ($app, $publishers) { display_result($app, $publishers->getAll()); });
+$app->get('/publishers/:id', function ($id) use ($app, $publishers) { display_result($app, $publishers->get($app, $id)); });
+$app->delete('/publishers/:id', function ($id) use ($app, $publishers) { $publishers->delete($app, $id); });
+$app->put('/publishers/:id', function ($id) use ($app, $publishers) { $publishers->put($app, $id); });
+$app->post('/publishers/', function () use ($app, $publishers) { $publishers->post($app); });
 
 $app->run();
 
