30879cc1c0f7362ae775421470e862b11e8d34af
Camille Coueslant <c.coueslant@criteo.com>
2015-07-09 15:10:54 +0200

    [ConfigUI] Add google/identity-toolkit-php-client library

--- configuration-ui/src/api/vendor/composer/autoload_real.php
+++ configuration-ui/src/api/vendor/composer/autoload_real.php
@@ -23,6 +23,10 @@ class ComposerAutoloaderInit6620e2dbbac494c8d78bbd0a28b3c8d8
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit6620e2dbbac494c8d78bbd0a28b3c8d8', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        array_push($includePaths, get_include_path());
+        set_include_path(join(PATH_SEPARATOR, $includePaths));
+
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
