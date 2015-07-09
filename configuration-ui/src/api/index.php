2b0a56627ab28c8d4744d77f9e4f1158057104f2
Manitra Andriamitondra <m.andriamitondra@criteo.com>
2015-07-09 11:11:12 +0200

    Add 3h of cache on bidder and advertiser configs

--- configuration-ui/src/api/index.php
+++ configuration-ui/src/api/index.php
@@ -17,14 +17,20 @@ $db->connect($config['db_user'], $config['db_password'], $config['db_name']);
 
 $bidders = new Bidders($db);
 $app->get('/bidders/', function () use ($app, $bidders) { display_result($app, $bidders->getAll($app)); });
-$app->get('/bidders/:id', function ($id) use ($app, $bidders) { display_result($app, $bidders->get($app, $id)); });
+$app->get('/bidders/:id', function ($id) use ($app, $bidders) { 
+	$app->expires('+3 hour');
+	display_result($app, $bidders->get($app, $id)); 
+});
 $app->delete('/bidders/:id', function ($id) use ($app, $bidders) { $bidders->delete($app, $id); });
 $app->put('/bidders/:id', function ($id) use ($app, $bidders) { $bidders->put($app, $id); });
 $app->post('/bidders/', function () use ($app, $bidders) { $bidders->post($app); });
 
 $publishers = new Publishers($db);
 $app->get('/publishers/', function () use ($app, $publishers) { display_result($app, $publishers->getAll($app)); });
-$app->get('/publishers/:id', function ($id) use ($app, $publishers) { display_result($app, $publishers->get($app, $id)); });
+$app->get('/publishers/:id', function ($id) use ($app, $publishers) { 
+	$app->expires('+3 hour');
+	display_result($app, $publishers->get($app, $id)); 
+});
 $app->delete('/publishers/:id', function ($id) use ($app, $publishers) { $publishers->delete($app, $id); });
 $app->put('/publishers/:id', function ($id) use ($app, $publishers) { $publishers->put($app, $id); });
 $app->post('/publishers/', function () use ($app, $publishers) { $publishers->post($app); });
