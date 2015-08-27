6efda2cbdb7ca4aa655128ba247003011455360b
Benoit Jehanno <b.jehanno@criteo.com>
2015-08-27 10:55:13 +0200

    Cleaning publisher-stats
    - get highchart with bower
    - moved draw function in controller

--- configuration-ui/src/js/controllers/publisher-stats.js
+++ configuration-ui/src/js/controllers/publisher-stats.js
@@ -1,66 +1,5 @@
 'use strict';
 
-
-function draw(rows) {
-
-	var dataDisplays = [];
-	for (var i=0 ; i<rows.length ; i++) {
-	  dataDisplays.push([new Date(rows[i].date).getTime(), rows[i].impressions]);
-	}
-
-	var dataRpm = [];
-	for (var i=0 ; i<rows.length ; i++) {
-	  dataRpm.push([new Date(rows[i].date).getTime(), rows[i].revenue]);
-	}
-
-	new Highcharts.Chart({
-		chart: {
-			type: 'line',
-			animation: false,
-			renderTo: 'container' },
-		title: {
-			text: 'Impressions and RPM'
-		},
-		xAxis: {
-			type: 'datetime',
-			dateTimeLabelFormats: { // don't display the dummy year
-				month: '%e. %b',
-				year: '%b'
-			}
-		},
-		plotOptions: {
-		},
-		yAxis: [{
-			title: {
-				text: 'Impressions' },
-				min: 0
-		}, {
-			title: {
-				text: 'RPM' },
-				opposite: true,
-				min:0
-		}],
-		tooltip:  {
-            shared: true
-		},
-
-		series: [
-		  {
-			name:'Impressions',
-			data:dataDisplays
-		  },
-		  {
-			name:'RPM',
-			yAxis: 1,
-			data:dataRpm
-		  }
-		]
-	});
-}
-
-
-
-
 angular.module('btApp.publisherStats', ['ui.router'])
 
 .config(['$stateProvider', function($stateProvider) {
@@ -74,13 +13,72 @@ angular.module('btApp.publisherStats', ['ui.router'])
 
 .controller('PublisherStatCtrl', ['$scope', '$q', '$http', '$stateParams', function($scope, $q, $http, $stateParams) {
 
+    function draw(rows) {
+        var dataDisplays = [];
+        for (var i=0 ; i<rows.length ; i++) {
+          dataDisplays.push([new Date(rows[i].date).getTime(), rows[i].impressions]);
+        }
+
+        var dataRpm = [];
+        for (var i=0 ; i<rows.length ; i++) {
+          dataRpm.push([new Date(rows[i].date).getTime(), rows[i].revenue]);
+        }
+
+        new Highcharts.Chart({
+            chart: {
+                type: 'line',
+                animation: false,
+                renderTo: 'container' },
+            title: {
+                text: 'Impressions and RPM'
+            },
+            xAxis: {
+                type: 'datetime',
+                dateTimeLabelFormats: { // don't display the dummy year
+                    month: '%e. %b',
+                    year: '%b'
+                }
+            },
+            plotOptions: {
+            },
+            yAxis: [{
+                title: {
+                    text: 'Impressions' },
+                    min: 0
+            }, {
+                title: {
+                    text: 'RPM' },
+                    opposite: true,
+                    min:0
+            }],
+            tooltip:  {
+                shared: true
+            },
+
+            series: [
+              {
+                name:'Impressions',
+                data:dataDisplays
+              },
+              {
+                name:'RPM',
+                yAxis: 1,
+                data:dataRpm
+              }
+            ]
+        });
+    }
+
     var now = new Date();
+
     var to = new Date(
         now.getFullYear(),
         now.getMonth(),
         now.getDate()
     );
+
     var from = new Date();
+
     from.setDate(to.getDate()-30);
     to.setDate(to.getDate()+1);
 
@@ -92,6 +90,7 @@ angular.module('btApp.publisherStats', ['ui.router'])
         },
         rows: [],
     };
+    
     if($stateParams['publisherId']) {
         $http
             .get("api/stats/publishers/"+ $stateParams['publisherId'] + "/" + (from.getTime() / 1000) + "/" + (to.getTime() / 1000))
